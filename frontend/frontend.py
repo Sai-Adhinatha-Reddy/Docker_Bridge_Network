71170aeb6426fccdea8bc088586ee88cc8e61515
Sai-Adhinatha-Reddy <91363558+Sai-Adhinatha-Reddy@users.noreply.github.com>
2025-04-20 15:59:18 -0500

    Update frontend.py

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -15,3 +15,17 @@ while True:
         print(f"Error connecting to backend: {e}")
     
     time.sleep(10)
+
+"""
+import requests
+
+backend_url = "http://backend-container:5000"
+
+response = requests.get(backend_url)
+
+if response.status_code == 200:
+    print("Frontend received the following response from the backend:")
+    print(response.text)
+else:
+    print("Failed to communicate with the backend.")
+"""
